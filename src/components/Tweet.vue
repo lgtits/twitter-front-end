6ecfd9f757404be14f62713f33022c60e83354fd
hongshengliu <hongshengliu@liontravel.com>
2022-02-23 22:49:41 +0800

    新增tweet組件

--- src/components/Tweet.vue
+++ src/components/Tweet.vue
@@ -5,21 +5,21 @@
         </figure>
         <div class="tweet-body">
             <div class="tweet-name">
-            <div class="name">Apple</div>
-            <div class="account-name">@apple</div>
-            <div class="update-time">. 3小時</div>
-        </div>
-        <div class="tweet-content">Nulla Lorem mollit cupidatat irure. Laborum magna nulla duis ullamco cillum dolor. Voluptate exercitation incididunt aliquip deserunt reprehenderit elit laborum. </div>
-        <div class="reaction">
-            <div class="comments">
-                <i>ICON</i>
-                <p>13</p>
+                <div class="name">Apple</div>
+                <div class="account-name">@apple</div>
+                <div class="update-time">. 3小時</div>
             </div>
-            <div class="favorite">
-                <i>ICON</i>
-                <p>76</p>
+            <div class="tweet-content">Nulla Lorem mollit cupidatat irure. Laborum magna nulla duis ullamco cillum dolor. Voluptate exercitation incididunt aliquip deserunt reprehenderit elit laborum. </div>
+            <div class="reaction">
+                <div class="comments">
+                    <i><img src="../assets/image/message.svg" alt="評論符號"></i>
+                    <p>13</p>
+                </div>
+                <div class="favorite">
+                    <i><img src="../assets/image/favorite.svg" alt="最愛符號"></i>
+                    <p>76</p>
+                </div>
             </div>
-        </div>
         </div>
     </div>
 </template>
@@ -33,7 +33,10 @@
         display: grid;
         grid-template-columns: 50px 1fr;
         grid-template-rows: 1fr;
+        gap: 10px;
         padding: 15px;
+        background-color: $white;
+        border: 1px solid $border;
         >figure{
             position: relative;
             width: 100%;
@@ -43,6 +46,34 @@
             overflow: hidden;
             >img{
                 @include poaCenter;
+            }
+        }
+        .tweet-body{
+            display: flex;
+            flex-direction: column;
+            .tweet-name{
+                display: flex;
+            }
+        }
+        .tweet-name{
+            margin-bottom: 5px;
+        }
+        .tweet-content{
+            margin-bottom: 15px;
+        }
+        .reaction{
+            display: flex;
+            .comments{
+                display: flex;
+                align-items: center;
+                margin-right: 50px;
+            }
+            .favorite{
+                display: flex;
+                align-items: center;
+            }
+            i{
+                margin-right: 10px;
             }
         }
     }
