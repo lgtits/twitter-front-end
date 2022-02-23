418b96a023ee81d001abcc55325fe93637d625fc
lgtits <a0937370704@gmail.com>
2022-02-23 15:10:38 +0800

    add adminTweets & adminUsers

--- src/views/Setting.vue
+++ src/views/Setting.vue
@@ -5,11 +5,9 @@
     </div>
     <main>
       <div class="header">
-        <h1>
-          帳戶設定
-        </h1>
+        <h1>帳戶設定</h1>
       </div>
-      <form class="main-form" >
+      <form class="main-form">
         <div class="input-box">
           <label for="account">帳號</label>
           <input
@@ -21,7 +19,7 @@
             autocomplete="username"
             required
             autofocus
-          >
+          />
         </div>
         <div class="input-box">
           <label for="account">名稱</label>
@@ -34,7 +32,7 @@
             autocomplete="name"
             required
             autofocus
-          >
+          />
         </div>
         <div class="input-box">
           <label for="account">Email</label>
@@ -47,7 +45,7 @@
             autocomplete="email"
             required
             autofocus
-          >
+          />
         </div>
         <div class="input-box">
           <label for="password">密碼</label>
@@ -59,7 +57,7 @@
             class="form-control"
             autocomplete="current-password"
             required
-          >
+          />
         </div>
         <div class="input-box">
           <label for="passwordCheck">密碼確認</label>
@@ -70,118 +68,118 @@
             type="password"
             class="form-control"
             required
-          >
+          />
         </div>
-        <button
-          class="save"
-          type="submit"
-        >
-          儲存
-        </button>
+        <button class="save" type="submit">儲存</button>
       </form>
     </main>
-    
   </div>
 </template>
 
 <script>
-import Navbar from './../components/Navbar'
+import Navbar from "./../components/Navbar";
 export default {
   components: {
-    Navbar
+    Navbar,
   },
-  data () {
+  data() {
     return {
-      account: '',
-      name: '',
-      email: '',
-      password: '',
-      passwordCheck: ''
-    }
+      account: "",
+      name: "",
+      email: "",
+      password: "",
+      passwordCheck: "",
+    };
   },
   methods: {
-    handleSubmit () {
+    handleSubmit() {
       const data = JSON.stringify({
         email: this.account,
-        password: this.password
-      })
+        password: this.password,
+      });
       // TODO: 向後端驗證使用者登入資訊是否合法
-      console.log('data', data)
-    }
-  }
-}
+      console.log("data", data);
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-  .container{
-    display: grid;
-    grid-template-columns: auto 600px auto;
-    margin: 0 auto;
+.container {
+  display: grid;
+  grid-template-columns: auto 600px auto;
+  grid-template-rows: auto;
+  grid-template-areas: "navbar main main ";
+  height: 100vh;
+  position: relative;
+  .left-column {
     position: relative;
-    .left-column{
+  }
+  main {
+    border: 1px solid #e6ecf0;
+    grid-area: main;
+    .header {
+      height: 55px;
+      border-bottom: 1px solid #e6ecf0;
       position: relative;
-    }
-    main{
-      border: 1px solid #E6ECF0;
-      .header{
-        height: 55px;
-        border-bottom: 1px solid #E6ECF0;
-        position: relative;
-        h1{
-          font-size: 19px;
-          position: absolute;
-          top: 13px;
-          left: 20px;
-        }
-      }
-    }
-    form{
-      display: flex;
-      flex-direction: column;
-      width: 540px;
-      margin: 30px auto auto auto;
-      .input-box{
-        position: relative;
-        margin-bottom: 32px;
-        background-color:#F5F8FA;
-        width: 540px;
-        border-bottom: solid 2px #657786;
-        border-radius: 0px 0px 4px 4px;
-        label{
-          font-weight: 500;
-          font-size: 15px;
-          line-height: 15px;
-          color: #657786;
-          position: absolute;
-          left: 10px;
-          top: 5px;
-        }
-      }
-      #account, #name, #email, #password, #passwordCheck{
-        padding: 20px 0 0 10px;
-        height: 52px;
-        background-color:#F5F8FA;
-        border: none;
+      h1 {
         font-size: 19px;
-        line-height: 28px;
-        &:focus {
-          outline: none;
-        }
-      }
-      .save{
-        margin: 10px auto 20px auto;
-        width: 116px;
-        height: 46px;
-        background: $orange;
-        border-radius: 50px;
-        color: white;
-        font-size: 18px;
-        font-weight: bold;
-        line-height: 26px;
-        text-align: center;
-        margin-right: 0;
+        position: absolute;
+        top: 13px;
+        left: 20px;
       }
     }
   }
-  
+  form {
+    display: flex;
+    flex-direction: column;
+    width: 540px;
+    margin: 30px auto auto 16px;
+    .input-box {
+      position: relative;
+      margin-bottom: 32px;
+      background-color: #f5f8fa;
+      width: 540px;
+      border-bottom: solid 2px #657786;
+      border-radius: 0px 0px 4px 4px;
+      label {
+        font-weight: 500;
+        font-size: 15px;
+        line-height: 15px;
+        color: #657786;
+        position: absolute;
+        left: 10px;
+        top: 5px;
+      }
+    }
+    #account,
+    #name,
+    #email,
+    #password,
+    #passwordCheck {
+      padding: 20px 0 0 10px;
+      height: 52px;
+      background-color: #f5f8fa;
+      border: none;
+      font-size: 19px;
+      line-height: 28px;
+      &:focus {
+        outline: none;
+      }
+    }
+    .save {
+      margin: 10px auto 20px auto;
+      width: 116px;
+      height: 46px;
+      background: $orange;
+      border-radius: 50px;
+      color: white;
+      font-size: 18px;
+      font-weight: bold;
+      line-height: 26px;
+      text-align: center;
+      margin-right: 0;
+    }
+  }
+}
 </style>
