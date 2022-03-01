4d335d58002353362aa8c6be5313b8e67c0599e8
lgtits <a0937370704@gmail.com>
2022-03-01 14:07:23 +0800

    deploy test #2

--- src/components/TweetContent.vue
+++ src/components/TweetContent.vue
@@ -1,116 +1,114 @@
 <template>
-    <div>
-        <PersonalCard :initUser="user"/>
-        <UserNavTabs/>
-        <Tweet v-for="tweet in tweets" :key="tweet.id" :initTweet="tweet"/>
-    </div>
+  <div>
+    <PersonalCard :initUser="user" />
+    <UserNavTabs />
+    <Tweet v-for="tweet in tweets" :key="tweet.id" :initTweet="tweet" />
+  </div>
 </template>
 
 <script>
-import Tweet from '../components/Tweet.vue' 
-import UserNavTabs from '../components/UserNavTabs.vue'
-import UsersApi from '../apis/user'
-import PersonalCard from '../components/PersonalCard.vue'
-import { Toast }  from '../utils/helpers'
-
+import Tweet from "../components/Tweet.vue";
+import UserNavTabs from "../components/UserNavTabs.vue";
+// import UsersApi from '../apis/user'
+import PersonalCard from "../components/PersonalCard.vue";
+import { Toast } from "../utils/helpers";
 
 export default {
-    name: 'TweetContent',
-    components: {
-        Tweet,
-        PersonalCard,
-        UserNavTabs
-    },
-    data(){
-        return {
-            tweets: [],
-            user: {
-                id: -1,
-                email: "",
-                password: "",
-                name: "",
-                account: "",
-                role: "",
-                avatar: "",
-                introduction: "",
-                cover: "",
-                tweetCount: null,
-                followingCount: null,
-                followerCount: null,
-                likedCount: null,
-                createdAt: "",
-                updatedAt: "",
-            }
+  name: "TweetContent",
+  components: {
+    Tweet,
+    PersonalCard,
+    UserNavTabs,
+  },
+  data() {
+    return {
+      tweets: [],
+      user: {
+        id: -1,
+        email: "",
+        password: "",
+        name: "",
+        account: "",
+        role: "",
+        avatar: "",
+        introduction: "",
+        cover: "",
+        tweetCount: null,
+        followingCount: null,
+        followerCount: null,
+        likedCount: null,
+        createdAt: "",
+        updatedAt: "",
+      },
+    };
+  },
+  methods: {
+    async fetchTweets(userId) {
+      console.log(userId);
+      try {
+        const { data, statusText } = await UsersApi.getUserTweets({ userId });
+        //    console.log('**',data.data)
+        if (statusText !== "OK") {
+          throw new Error(statusText);
         }
+        this.tweets = data.data;
+      } catch (error) {
+        console.log("error", error.message);
+        Toast.fire({
+          icon: "error",
+          title: "未能取得使用者推文，請稍後在試",
+        });
+      }
     },
-    methods: {
-        async fetchTweets(userId){
-            try{
-               const {data, statusText} = await UsersApi.getUserTweets({userId})
-            //    console.log('**',data.data)
-               if(statusText !== 'OK'){
-                   throw new Error(statusText)
-               }
-               this.tweets = data.data
-               
-            }catch(error){
-                console.log('error',error.message)
-                Toast.fire({
-                    icon: 'error',
-                    title: '未能取得使用者推文，請稍後在試'
-                })
-            }
-        },
-        async fetchUser(userId){
-            try{
-                const {data, statusText} = await UsersApi.getUser({userId})
-                console.log('$$',data)
-                const {
-                    id,
-                    email,
-                    name,
-                    account,
-                    role,
-                    avatar,
-                    introduction,
-                    cover,
-                    tweetCount,
-                    followingCount,
-                    followerCount,
-                    likedCount,
-                    createdAt,
-                    updatedAt,
-                } = data.userData
-                if(statusText !== 'OK'){
-                    throw new Error(statusText)
-                }
-                this.user= {
-                    ...this.user,
-                    id,
-                    email,
-                    name,
-                    account,
-                    role,
-                    avatar,
-                    introduction,
-                    cover,
-                    tweetCount,
-                    followingCount,
-                    followerCount,
-                    likedCount,
-                    createdAt,
-                    updatedAt,
-                }
-
-            }catch(error){
-                console.log('error',error)
-            }
-        },
+    async fetchUser(userId) {
+      try {
+        const { data, statusText } = await UsersApi.getUser({ userId });
+        console.log("$$", data);
+        const {
+          id,
+          email,
+          name,
+          account,
+          role,
+          avatar,
+          introduction,
+          cover,
+          tweetCount,
+          followingCount,
+          followerCount,
+          likedCount,
+          createdAt,
+          updatedAt,
+        } = data.userData;
+        if (statusText !== "OK") {
+          throw new Error(statusText);
+        }
+        this.user = {
+          ...this.user,
+          id,
+          email,
+          name,
+          account,
+          role,
+          avatar,
+          introduction,
+          cover,
+          tweetCount,
+          followingCount,
+          followerCount,
+          likedCount,
+          createdAt,
+          updatedAt,
+        };
+      } catch (error) {
+        console.log("error", error);
+      }
     },
-    created(){
-        const id = this.$route.params.id
-        this.fetchUser(id)
-        this.fetchTweets(id)
-    }
-}
+  },
+  created() {
+    const id = this.$route.params.id;
+    this.fetchUser(id);
+    this.fetchTweets(id);
+  },
+};
 </script>
