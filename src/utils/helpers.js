7dd90c555eb01828400e6a6253042abfd9df612b
lgtits <a0937370704@gmail.com>
2022-03-03 14:11:53 +0800

    cover avatar preview

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -2,10 +2,10 @@ import axios from 'axios'
 import Swal from 'sweetalert2'
 
 //本機串接api
-// const baseURL = 'http://localhost:3000/api'
+const baseURL = 'http://localhost:3000/api'
 
 // github pages
-const baseURL = 'https://sheltered-ravine-31386.herokuapp.com/api'
+// const baseURL = 'https://sheltered-ravine-31386.herokuapp.com/api'
 
 export const apiHelper = axios.create({
     baseURL
